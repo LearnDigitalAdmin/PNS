7133ec7564c02140d2fc454d2fe5ac83bf0b75dd
Sam Muhia <sammyem999@gmail.com>
2026-06-18 22:25:03 +0300

    First Commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuEPDV2wpBtgfSW6L4v05PVUxeaFkW3k0",
+  authDomain: "plot-9fd6e.firebaseapp.com",
+  projectId: "plot-9fd6e",
+  storageBucket: "plot-9fd6e.firebasestorage.app",
+  messagingSenderId: "1037620305589",
+  appId: "1:1037620305589:web:af0047b9a1b65ec5b068fc",
+  measurementId: "G-WPNH7458SC"
+};
+
+const app  = initializeApp(firebaseConfig);
+export const db      = getFirestore(app);
+export const auth    = getAuth(app);
+export const storage = getStorage(app);
